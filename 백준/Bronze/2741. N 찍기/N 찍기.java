6e838eec1adf044b05f6cee31bceba1266ceaf2a
Jhong <100734717+jhong810@users.noreply.github.com>
2024-06-18 14:54:18 +0900

    [Bronze V] Title: N 찍기, Time: 1200 ms, Memory: 33812 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        for (int i=0;i<x;i++){
+            System.out.println(i+1);
+        }
+    }
+}
