ec6ba5f22cfa287597cb4024ef1d963f99672cc3
Jhong <100734717+jhong810@users.noreply.github.com>
2024-06-18 14:59:12 +0900

    [Bronze V] Title: ??!, Time: 216 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a;
+        a= scan.nextLine();
+
+        System.out.println(a+"??!");
+
+
+    }
+}
