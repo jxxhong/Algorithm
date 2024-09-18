353374a9d5044e444bb80bae56349cd858397b2b
Jaehong Ahn <100734717+jxxhong@users.noreply.github.com>
2024-09-19 00:12:00 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -0,0 +1,8 @@
+function solution(a, d, included) {
+    let answer = 0;
+    for(let i = 0; i < included.length; i++){
+        if(included[i]) answer += a;
+        a += d;
+    }
+    return answer;
+}
