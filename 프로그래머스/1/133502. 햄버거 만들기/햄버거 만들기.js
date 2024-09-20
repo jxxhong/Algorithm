05d479b42e93136363da98a3099845482daa6e06
Jaehong Ahn <100734717+jxxhong@users.noreply.github.com>
2024-09-20 22:07:55 +0900

    [level 1] Title: 햄버거 만들기, Time: 198.07 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -5,9 +5,8 @@ function solution(ingredient) {
     for (let i = 0; i < ingredient.length; i++) {
         stack.push(ingredient[i]);
         
-        // 스택의 마지막 4개 요소가 1, 2, 3, 1인지 확인
         if (stack.length >= 4) {
-            let lastFour = stack.slice(-4);  // 스택의 마지막 4개를 확인
+            let lastFour = stack.slice(-4);
             if (lastFour.join('') === '1231') {
                 stack.splice(-4, 4);
                 answer++;
